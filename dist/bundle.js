d15442a2b593603600abc0068330571890757629
mingming <jimin23623@gmail.com>
2023-08-30 16:57:41 +0900

    rollup js 개발환경 세팅

---
+++ dist/bundle.js
@@ -0,0 +1,26 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+'use strict';
+
+
+
+function ___$insertStylesToHeader(css) {
+  if (!css) {
+    return
+  }
+  if (typeof window === 'undefined') {
+    return
+  }
+
+  const style = document.createElement('style');
+
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+  return css
+}
+
+___$insertStylesToHeader(".container {\n  width: 300px;\n  height: 300px;\n  background: red;\n}");
+
+console.log("todo list");
+//# sourceMappingURL=bundle.js.map
