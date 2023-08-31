6e274d4d19ed3e84da9ac57bb0fbc260bd932f4f
mingming <jimin23623@gmail.com>
2023-08-31 21:48:51 +0900

    localstorage에 저장 완료

--- src/js/index.js
+++ src/js/index.js
@@ -44,10 +44,60 @@ class Router {
     }
 }
 
+class Storage {
+    saveTodo(id, todoContent) {
+        // localStorage에 저장된 혹은 빈값들을 todosData에 할당
+        const todosData = this.getTodos();
+        // todosData에 저장할 값(id, 내용, 상태)들을 넘겨 줄 값들을 준비한다.
+        todosData.push({ id, content: todoContent, status: "TODO" });
+        // 준비된 값들을 localStorage의 todos에 넣을거고, 객체를 반드시 string화 해서 보낸다.
+        localStorage.setItem("todos", JSON.stringify(todosData));
+    }
+    editTodo(id, todoContent, status = "TODO") {
+        const todosData = this.getTodos();
+        // target todo의 index를 가져와 저장한다.
+        const todoIndex = todosData.findIndex((todo) => todo.id == id);
+        // 저정되어진 todo들 중에 target index와 같은 애를 가져와 저장한다.
+        const targetTodoData = todosData[todoIndex];
+        const editedTodoData =
+            todoContent === ""
+                ? // todoContent에 string이 없다면? 기존 content를 가져오기만 한뒤 status만 바꿔준다.
+                  { ...targetTodoData, status }
+                : // todoContent에 string 있다면? content 넣어준다.
+                  { ...targetTodoData, content: todoContent };
+        // 수정할 todo 영역을 삭제한 다음 거기다가 수정된 데이터를 넣어준다.
+        todosData.splice(todoIndex, 1, editedTodoData);
+        // 해당 값들을 다시 localStorage에 string으로 저장해 주면서 마무리.
+        localStorage.setItem("todos", JSON.stringify(todosData));
+    }
+    deleteTodo(id) {
+        const todosData = this.getTodos();
+        todosData.splice(
+            // todosData에서 삭제하고 싶은 id값과 같은 애를 먼저 찾아서 잘라준다
+            todosData.findIndex((todo) => todo.id == id),
+            1
+        );
+        // 잘린 상태의 todosData를 다시 string화 시켜서 localStorage에 저장해 준다.
+        localStorage.setItem("todos", JSON.stringify(todosData));
+    }
+    getTodos() {
+        // localStorage에 todos를 가져오는데 값이 null이면 빈 값 넘겨주고, 값이 있다면 string화 된 데이터를 파싱해준다.
+        return localStorage.getItem("todos") === null
+            ? []
+            : JSON.parse(localStorage.getItem("todos"));
+    }
+}
+
 class TodoList {
-    constructor() {
+    constructor(storage) {
+        this.initStorage(storage);
         this.assignElement();
         this.addEvent();
+        this.loadSavedData();
+    }
+
+    initStorage(storage) {
+        this.storage = storage;
     }
 
     assignElement() {
@@ -70,6 +120,15 @@ class TodoList {
             this.onClickTodoList.bind(this)
         );
         this.addRadioBtnEvent();
+    }
+
+    loadSavedData() {
+        // storage class에 todos 데이터 있는지 없는지 판단하여 가져오는 함수 불러옴.
+        const todosData = this.storage.getTodos();
+        for (const todoData of todosData) {
+            const { id, content, status } = todoData;
+            this.createTodoElement(id, content, status);
+        }
     }
 
     addRadioBtnEvent() {
@@ -131,6 +190,12 @@ class TodoList {
     completeTodo(target) {
         const todoDiv = target.closest(".todo");
         todoDiv.classList.toggle("done");
+        const { id } = todoDiv.dataset;
+        this.storage.editTodo(
+            id,
+            "",
+            todoDiv.classList.contains("done") ? "DONE" : "TODO"
+        );
     }
 
     saveTodo(target) {
@@ -138,6 +203,8 @@ class TodoList {
         todoDiv.classList.remove("edit");
         const todoInputEl = todoDiv.querySelector("input");
         todoInputEl.readOnly = true;
+        const { id } = todoDiv.dataset;
+        this.storage.editTodo(id, todoInputEl.value);
     }
 
     editTodo(target) {
@@ -154,6 +221,9 @@ class TodoList {
             todoDiv.remove();
         });
         todoDiv.classList.add("delete");
+
+        // createTodoElement 메서드에서 각 div에 부여해준 data-set id 값을 가져와서 삭제한다.
+        this.storage.deleteTodo(todoDiv.dataset.id);
     }
 
     onClickAddBtn() {
@@ -162,12 +232,21 @@ class TodoList {
             return;
         }
 
-        this.createTodoElement(this.todoInputEl.value);
+        const id = Date.now(); // 현재 시간을 밀리세컨즈 단위로 나타내주는데 이를 id값으로 저장
+        this.storage.saveTodo(id, this.todoInputEl.value);
+
+        this.createTodoElement(id, this.todoInputEl.value);
     }
 
-    createTodoElement(value) {
+    createTodoElement(id, value, status = null) {
         const todoDiv = document.createElement("div");
         todoDiv.classList.add("todo");
+
+        if (status === "DONE") {
+            todoDiv.classList.add("done");
+        }
+
+        todoDiv.dataset.id = id;
 
         const todoContent = document.createElement("input");
         todoContent.value = value;
@@ -210,7 +289,7 @@ class TodoList {
 // document에 DOM이 다 로드된 다음에 인스턴스 생성
 document.addEventListener("DOMContentLoaded", () => {
     const router = new Router();
-    const todoList = new TodoList();
+    const todoList = new TodoList(new Storage());
     const routerCallback = (status) => () => {
         todoList.filterTodo(status);
         document.querySelector(
